4cd31faffd16949d608f75f248369e2509bd80a2
Yaroslav <qwerty_qwerty12345678910@mail.ru>
2024-11-13 18:28:36 +0300

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+namespace ConsoleTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var fileName = "MLstartConfig.json";
+        var content = @"{""Test"": ""123""}";
+        File.WriteAllText(fileName, content);
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory
+            .GetCurrentDirectory())
+            .AddJsonFile(fileName).Build();
+        string testConfig = configuration["Test"];
+        Console.WriteLine(testConfig);
+    }
+}
