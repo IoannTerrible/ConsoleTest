290f0d39b4a004ef3f51f6fe7e913865bc64496b
Yaroslav <qwerty_qwerty12345678910@mail.ru>
2024-11-13 18:36:52 +0300

    Test

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,14 +6,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var fileName = "111111111111111MLstartConfig.json";
-        var content = @"111111111111111{""Test"": ""1234556343467""}";
-        File.WriteAllTex111111111111111t(fileName, content);
-        IConfiguration c111111111111111onfiguration = new ConfigurationBuilder()
-            .SetBasePath111111111111111(Directory
-            .GetCurrentD111111111111111irectory())
-            .AddJsonFile111111111111111(fileName).Build();
-        string testConfi111111111111111g = configuration["Test"];
-        Console.WriteLin111111111111111e(testConfig);
+        var fileName = "MLstartConfig.json";
+        var content = @"{""Test"": ""1234556343467""}";
+        File.WriteAllText(fileName, content);
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory
+            .GetCurrentDirectory())
+            .Add(fileName).Build();
+        string testConfig = configuration["Test"];
+        Console.WriteLine(testConfig);
     }
 }
